8b55adff6451df88fd009caf4a2aa2975d02fdad
vbreugel <v.breugel@presspage.com>
2017-10-24 17:37:42 +0200

    update scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,17 @@
 // including plugins
 var gulp = require('gulp');
-var minifyHtml = require("gulp-minify-html");
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
-var changed = require('gulp-changed');
 
 
 gulp.task('sass', function () {
-    return gulp.src('./theme.scss')
-        .pipe(changed('build'))
+    return gulp.src('./scss/app.scss')
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(autoprefixer())
-        .pipe(gulp.dest('./'));
+        .pipe(gulp.dest('css/'));
 });
 
 //Watch task
 gulp.task('default',function() {
-    gulp.watch('./theme.scss',['sass']);
+    gulp.watch('./scss/**/*.scss',['sass']);
 });
